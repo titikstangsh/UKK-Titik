29f555550f553fdc45fc5f4ab2c57e8a8c625cdd
titikstangsh <102141709+titikstangsh@users.noreply.github.com>
2022-03-22 14:54:31 +0700

    Add files via upload

---
+++ keripik.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2022 at 08:50 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `keripik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` varchar(100) NOT NULL,
+  `id_barang` varchar(100) NOT NULL,
+  `jumlahtersedia` int(11) NOT NULL,
+  `jumlahterjual` int(11) NOT NULL,
+  `stokakhir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `jumlahtersedia`, `jumlahterjual`, `stokakhir`) VALUES
+('01', '01', 100, 10, 90);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbarang`
+--
+
+CREATE TABLE `tblbarang` (
+  `id_barang` varchar(100) NOT NULL,
+  `jeniskeripik` varchar(100) NOT NULL,
+  `rasakeripik` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblbarang`
+--
+
+INSERT INTO `tblbarang` (`id_barang`, `jeniskeripik`, `rasakeripik`, `harga`) VALUES
+('01', 'singkong', 'balado', 5000),
+('02', 'pisang', 'manis', 4000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD UNIQUE KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `tblbarang`
+--
+ALTER TABLE `tblbarang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tblbarang` (`id_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
